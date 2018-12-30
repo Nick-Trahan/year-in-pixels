809a90b1fbd6b2bea8dba303f99d089c853e9719
Nick-Trahan <nicktrahan@gmail.com>
2018-12-30 10:11:46 -0600

    Add condition to promptUser function that ignores bad clicks

--- script.js
+++ script.js
@@ -6,53 +6,60 @@ function promptUser(event) {
   let clickedBox = event.target;
   console.log(clickedBox.id);
 
-  let body = document.querySelector('body');
-
-  let panel = document.createElement('div');
-  panel.setAttribute('class', 'msgBox');
-  body.appendChild(panel);
-
-  let question = document.createElement('p');
-  question.setAttribute('id', 'question');
-  question.textContent = 'How\'s your mood today?';
-  panel.appendChild(question);
-
-  let ansFantastic = document.createElement('p');
-  // let btnFantastic = document.createElement('button');
-  ansFantastic.setAttribute('class', 'answer');
-  ansFantastic.textContent = 'Fantastic, amazing day! :D';
-  panel.appendChild(ansFantastic);
-
-  let ansSad = document.createElement('p');
-  ansSad.setAttribute('class', 'answer');
-  ansSad.textContent = 'Sad, gloomy day :\'(';
-  panel.appendChild(ansSad);
-
-  let ansNervous = document.createElement('p');
-  ansNervous.setAttribute('class', 'answer');
-  ansNervous.textContent = 'Nervous :/';
-  panel.appendChild(ansNervous);
-
-  let ansNormal = document.createElement('p');
-  ansNormal.setAttribute('class', 'answer');
-  ansNormal.textContent = 'Normal :|';
-  panel.appendChild(ansNormal);
-
-  let ansGreat = document.createElement('p');
-  ansGreat.setAttribute('class', 'answer');
-  ansGreat.textContent = 'Great Day :)';
-  panel.appendChild(ansGreat);
-
-  let ansMad = document.createElement('p');
-  ansMad.setAttribute('class', 'answer');
-  ansMad.textContent = 'Mad, frustrating day >:(';
-  panel.appendChild(ansMad);
-
-  let closeBtn = document.createElement('button');
-  closeBtn.textContent = 'x';
-  panel.appendChild(closeBtn);
-
-  closeBtn.onclick = function() {
-    panel.parentNode.removeChild(panel);
+  if (clickedBox.id === 'button-container') {
+    console.log('oops!');
+  } else {
+    colorBoxContainer.removeEventListener('click', promptUser);
+  
+    let body = document.querySelector('body');
+  
+    let panel = document.createElement('div');
+    panel.setAttribute('class', 'msgBox');
+    body.appendChild(panel);
+  
+    let question = document.createElement('p');
+    question.setAttribute('id', 'question');
+    question.textContent = 'How\'s your mood today?';
+    panel.appendChild(question);
+  
+    let ansFantastic = document.createElement('p');
+    // let btnFantastic = document.createElement('button');
+    ansFantastic.setAttribute('class', 'answer');
+    ansFantastic.textContent = 'Fantastic, amazing day! :D';
+    panel.appendChild(ansFantastic);
+  
+    let ansSad = document.createElement('p');
+    ansSad.setAttribute('class', 'answer');
+    ansSad.textContent = 'Sad, gloomy day :\'(';
+    panel.appendChild(ansSad);
+  
+    let ansNervous = document.createElement('p');
+    ansNervous.setAttribute('class', 'answer');
+    ansNervous.textContent = 'Nervous :/';
+    panel.appendChild(ansNervous);
+  
+    let ansNormal = document.createElement('p');
+    ansNormal.setAttribute('class', 'answer');
+    ansNormal.textContent = 'Normal :|';
+    panel.appendChild(ansNormal);
+  
+    let ansGreat = document.createElement('p');
+    ansGreat.setAttribute('class', 'answer');
+    ansGreat.textContent = 'Great Day :)';
+    panel.appendChild(ansGreat);
+  
+    let ansMad = document.createElement('p');
+    ansMad.setAttribute('class', 'answer');
+    ansMad.textContent = 'Mad, frustrating day >:(';
+    panel.appendChild(ansMad);
+  
+    let closeBtn = document.createElement('button');
+    closeBtn.textContent = 'x';
+    panel.appendChild(closeBtn);
+  
+    closeBtn.onclick = function() {
+      panel.parentNode.removeChild(panel);
+      colorBoxContainer.addEventListener('click', promptUser);
+    }
   }
 }
