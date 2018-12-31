3f508271bb925100def2f227e493cc9f79a50675
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2018-12-31 09:12:44 -0600

    Change if...else statment to switch case in promptUser function

--- script.js
+++ script.js
@@ -5,65 +5,69 @@ colorBoxContainer.addEventListener('click', promptUser);
 function promptUser(event) {
   let clickedBox = event.target;
 
-  if (clickedBox.id === 'button-container') {
-    console.log('oops!');
-  } else {
-    colorBoxContainer.removeEventListener('click', promptUser);
+  switch (clickedBox.className) {
+    case 'color-box':
+      colorBoxContainer.removeEventListener('click', promptUser);
   
-    let body = document.querySelector('body');
+      let body = document.querySelector('body');
   
-    let panel = document.createElement('div');
-    panel.setAttribute('class', 'msgBox');
-    body.appendChild(panel);
+      let panel = document.createElement('div');
+      panel.setAttribute('class', 'msgBox');
+      body.appendChild(panel);
   
-    let question = document.createElement('p');
-    question.setAttribute('id', 'question');
-    question.textContent = 'How\'s your mood today?';
-    panel.appendChild(question);
+      let question = document.createElement('p');
+      question.setAttribute('id', 'question');
+      question.textContent = 'How\'s your mood today?';
+      panel.appendChild(question);
   
-    let ansFantastic = document.createElement('p');
-    ansFantastic.setAttribute('id', 'p-fantastic');
-    ansFantastic.setAttribute('class', 'answer');
-    ansFantastic.textContent = 'Fantastic, amazing day! :D';
-    panel.appendChild(ansFantastic);
+      let ansFantastic = document.createElement('p');
+      ansFantastic.setAttribute('id', 'p-fantastic');
+      ansFantastic.setAttribute('class', 'answer');
+      ansFantastic.textContent = 'Fantastic, amazing day! :D';
+      panel.appendChild(ansFantastic);
   
-    let ansSad = document.createElement('p');
-    ansSad.setAttribute('id', 'p-sad');
-    ansSad.setAttribute('class', 'answer');
-    ansSad.textContent = 'Sad, gloomy day :\'(';
-    panel.appendChild(ansSad);
+      let ansSad = document.createElement('p');
+      ansSad.setAttribute('id', 'p-sad');
+      ansSad.setAttribute('class', 'answer');
+      ansSad.textContent = 'Sad, gloomy day :\'(';
+      panel.appendChild(ansSad);
   
-    let ansNervous = document.createElement('p');
-    ansNervous.setAttribute('id', 'p-nervous');
-    ansNervous.setAttribute('class', 'answer');
-    ansNervous.textContent = 'Nervous :/';
-    panel.appendChild(ansNervous);
+      let ansNervous = document.createElement('p');
+      ansNervous.setAttribute('id', 'p-nervous');
+      ansNervous.setAttribute('class', 'answer');
+      ansNervous.textContent = 'Nervous :/';
+      panel.appendChild(ansNervous);
   
-    let ansNormal = document.createElement('p');
-    ansNormal.setAttribute('id', 'p-normal');
-    ansNormal.setAttribute('class', 'answer');
-    ansNormal.textContent = 'Normal :|';
-    panel.appendChild(ansNormal);
+      let ansNormal = document.createElement('p');
+      ansNormal.setAttribute('id', 'p-normal');
+      ansNormal.setAttribute('class', 'answer');
+      ansNormal.textContent = 'Normal :|';
+      panel.appendChild(ansNormal);
   
-    let ansGreat = document.createElement('p');
-    ansGreat.setAttribute('id', 'p-great');
-    ansGreat.setAttribute('class', 'answer');
-    ansGreat.textContent = 'Great Day :)';
-    panel.appendChild(ansGreat);
+      let ansGreat = document.createElement('p');
+      ansGreat.setAttribute('id', 'p-great');
+      ansGreat.setAttribute('class', 'answer');
+      ansGreat.textContent = 'Great Day :)';
+      panel.appendChild(ansGreat);
   
-    let ansMad = document.createElement('p');
-    ansMad.setAttribute('id', 'p-mad');
-    ansMad.setAttribute('class', 'answer');
-    ansMad.textContent = 'Mad, frustrating day >:(';
-    panel.appendChild(ansMad);
+      let ansMad = document.createElement('p');
+      ansMad.setAttribute('id', 'p-mad');
+      ansMad.setAttribute('class', 'answer');
+      ansMad.textContent = 'Mad, frustrating day >:(';
+      panel.appendChild(ansMad);
   
-    let closeBtn = document.createElement('button');
-    closeBtn.textContent = 'x';
-    panel.appendChild(closeBtn);
+      let closeBtn = document.createElement('button');
+      closeBtn.textContent = 'x';
+      panel.appendChild(closeBtn);
   
-    closeBtn.onclick = function() {
-      panel.parentNode.removeChild(panel);
-      colorBoxContainer.addEventListener('click', promptUser);
-    }
+      closeBtn.onclick = function() {
+        panel.parentNode.removeChild(panel);
+        colorBoxContainer.addEventListener('click', promptUser);
+      }
+      break;
+  
+    default:
+      console.log('oops!');
+      break;
   }
 }
