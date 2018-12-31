fe1ec17ce94c708c4dbe142ddb57d8b87daa3821
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2018-12-31 11:14:32 -0600

    Edit classes on mood answers, Begin work on saveMoodChoice function

--- script.js
+++ script.js
@@ -22,46 +22,49 @@ function promptUser(event) {
   
       let ansFantastic = document.createElement('p');
       ansFantastic.setAttribute('id', 'p-fantastic');
-      ansFantastic.setAttribute('class', 'answer');
+      ansFantastic.setAttribute('class', 'mood-answer');
       ansFantastic.textContent = 'Fantastic, amazing day! :D';
       panel.appendChild(ansFantastic);
   
       let ansSad = document.createElement('p');
       ansSad.setAttribute('id', 'p-sad');
-      ansSad.setAttribute('class', 'answer');
+      ansSad.setAttribute('class', 'mood-answer');
       ansSad.textContent = 'Sad, gloomy day :\'(';
       panel.appendChild(ansSad);
   
       let ansNervous = document.createElement('p');
       ansNervous.setAttribute('id', 'p-nervous');
-      ansNervous.setAttribute('class', 'answer');
+      ansNervous.setAttribute('class', 'mood-answer');
       ansNervous.textContent = 'Nervous :/';
       panel.appendChild(ansNervous);
   
       let ansNormal = document.createElement('p');
       ansNormal.setAttribute('id', 'p-normal');
-      ansNormal.setAttribute('class', 'answer');
+      ansNormal.setAttribute('class', 'mood-answer');
       ansNormal.textContent = 'Normal :|';
       panel.appendChild(ansNormal);
   
       let ansGreat = document.createElement('p');
       ansGreat.setAttribute('id', 'p-great');
-      ansGreat.setAttribute('class', 'answer');
+      ansGreat.setAttribute('class', 'mood-answer');
       ansGreat.textContent = 'Great Day :)';
       panel.appendChild(ansGreat);
   
       let ansMad = document.createElement('p');
       ansMad.setAttribute('id', 'p-mad');
-      ansMad.setAttribute('class', 'answer');
+      ansMad.setAttribute('class', 'mood-answer');
       ansMad.textContent = 'Mad, frustrating day >:(';
       panel.appendChild(ansMad);
   
       let closeBtn = document.createElement('button');
       closeBtn.textContent = 'x';
       panel.appendChild(closeBtn);
+
+      panel.addEventListener('click', saveMoodChoice);
   
       closeBtn.onclick = function() {
         panel.parentNode.removeChild(panel);
+        panel.removeEventListener('click', saveMoodChoice);
         colorBoxContainer.addEventListener('click', promptUser);
       }
       break;
@@ -70,4 +73,9 @@ function promptUser(event) {
       console.log('oops!');
       break;
   }
+}
+
+function saveMoodChoice(event) {
+  let userMood = event.target;
+  console.log(userMood.id);
 }
