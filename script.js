f14dfdb8e1e846eee10bbc794b3101f00131b5d8
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2018-12-31 16:51:35 -0600

    Get basic functionality working for demo

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const colorBoxContainer = document.querySelector('#button-container');
 
-colorBoxContainer.addEventListener('click', dateChosen);
+let usersYear = [];
+
+colorBoxContainer.addEventListener('click', findTarget);
 colorBoxContainer.addEventListener('click', promptUser);
 
-function dateChosen(event) {
+function findTarget(event) {
   const clickedBox = event.target;
   return clickedBox;
 }
 
-function promptUser(validBox) {
-  validBox = dateChosen(event);
+function promptUser() {
+  let validBox = findTarget(event);
+
+  usersYear[0] = validBox;
 
   switch (validBox.className) {
     case 'color-box':
-      colorBoxContainer.removeEventListener('click', dateChosen);
+      colorBoxContainer.removeEventListener('click', findTarget);
       colorBoxContainer.removeEventListener('click', promptUser);
   
       let body = document.querySelector('body');
@@ -28,37 +32,37 @@ function promptUser(validBox) {
       panel.appendChild(question);
   
       let ansFantastic = document.createElement('p');
-      ansFantastic.setAttribute('id', 'p-fantastic');
+      ansFantastic.setAttribute('id', 'rebeccapurple');
       ansFantastic.setAttribute('class', 'mood-answer');
       ansFantastic.textContent = 'Fantastic, amazing day! :D';
       panel.appendChild(ansFantastic);
   
       let ansSad = document.createElement('p');
-      ansSad.setAttribute('id', 'p-sad');
+      ansSad.setAttribute('id', 'blue');
       ansSad.setAttribute('class', 'mood-answer');
       ansSad.textContent = 'Sad, gloomy day :\'(';
       panel.appendChild(ansSad);
   
       let ansNervous = document.createElement('p');
-      ansNervous.setAttribute('id', 'p-nervous');
+      ansNervous.setAttribute('id', 'green');
       ansNervous.setAttribute('class', 'mood-answer');
       ansNervous.textContent = 'Nervous :/';
       panel.appendChild(ansNervous);
   
       let ansNormal = document.createElement('p');
-      ansNormal.setAttribute('id', 'p-normal');
+      ansNormal.setAttribute('id', 'yellow');
       ansNormal.setAttribute('class', 'mood-answer');
       ansNormal.textContent = 'Normal :|';
       panel.appendChild(ansNormal);
   
       let ansGreat = document.createElement('p');
-      ansGreat.setAttribute('id', 'p-great');
+      ansGreat.setAttribute('id', 'orange');
       ansGreat.setAttribute('class', 'mood-answer');
       ansGreat.textContent = 'Great Day :)';
       panel.appendChild(ansGreat);
   
       let ansMad = document.createElement('p');
-      ansMad.setAttribute('id', 'p-mad');
+      ansMad.setAttribute('id', 'red');
       ansMad.setAttribute('class', 'mood-answer');
       ansMad.textContent = 'Mad, frustrating day >:(';
       panel.appendChild(ansMad);
@@ -67,14 +71,18 @@ function promptUser(validBox) {
       closeBtn.textContent = 'x';
       panel.appendChild(closeBtn);
 
-      panel.addEventListener('click', saveMoodChoice);
+      panel.addEventListener('click', findTarget);
+      panel.addEventListener('click', colorDate);
   
       closeBtn.onclick = function() {
-        panel.removeEventListener('click', saveMoodChoice);
+        panel.removeEventListener('click', findTarget);
+        panel.removeEventListener('click', colorDate);
         panel.parentNode.removeChild(panel);
 
-        colorBoxContainer.addEventListener('click', dateChosen);
+        colorBoxContainer.addEventListener('click', findTarget);
         colorBoxContainer.addEventListener('click', promptUser);
+
+        usersYear.pop();
       }
       break;
   
@@ -84,7 +92,21 @@ function promptUser(validBox) {
   }
 }
 
-function saveMoodChoice(event) {
-  const userMood = event.target;
-  console.log(userMood.id);
+function colorDate() {
+  let userMood = findTarget(event);
+  let panel = document.querySelector('.msgBox');
+
+  usersYear[1] = userMood.id;
+
+  panel.removeEventListener('click', findTarget);
+  panel.removeEventListener('click', colorDate);
+  panel.parentNode.removeChild(panel);
+
+  let date = usersYear[0];
+  let mood = usersYear[1];
+
+  date.style.backgroundColor = mood;
+
+  colorBoxContainer.addEventListener('click', findTarget);
+  colorBoxContainer.addEventListener('click', promptUser);
 }
